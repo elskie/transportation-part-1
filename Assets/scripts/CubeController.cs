3810fa26d3751cb8bb9efdf2eb072c31a30c5c5a
elskie <55069033+elskie@users.noreply.github.com>
2019-10-21 09:48:36 -0500

    Add files via upload
    I am unsure Ive been uploading things correctly so im gonna try doing it this way now

---
+++ Assets/scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {   if (GameController.ActiveCube != null) // null means that there is no value assigned to the variable
+        {
+            GameController.ActiveCube.GetComponent<Renderer>().material.color = Color.white;//what is currently the active cube is turned white
+        }
+        gameObject.GetComponent< Renderer > ().material.color = Color.red;
+        GameController.ActiveCube = gameObject; //the variable ActiveCube is now assigned to the cube that was just clicked
+    }
+}
